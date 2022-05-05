833a55b91fc580fed65f0b3751e75fd4fb2680ef
lingao <lingao@intelink.net>
2022-05-05 18:02:33 +0800

    实现帖子显示并分页

--- src/main.js
+++ src/main.js
@@ -10,7 +10,26 @@ import ElementUI from "element-ui"
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/assets/app.css'
 
+// import format from 'date-fns/format'
 import '@/permission'
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+// 国际化
+import 'dayjs/locale/zh-cn'
+const dayjs = require('dayjs');
+
+// 相对时间插件
+dayjs.extend(relativeTime)
+
+dayjs.locale('zh-cn') // use locale globally
+dayjs().locale('zh-cn').format() // use locale in a specific instance
+
+Vue.prototype.dayjs = dayjs;//可以全局使用dayjs
+
+// Vue.filter('date', (date) => {
+//   return format(new Date(date), 'yyyy-MM-dd')
+// })
+
 
 Vue.use(Buefy)
 Vue.use(ElementUI)
